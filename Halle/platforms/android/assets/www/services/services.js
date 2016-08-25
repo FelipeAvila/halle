f1948d89daa41d4c85389c93f04c9b6d07976a59
lbaiao <leonardobaiao@gmail.com>
2016-08-25 11:59:03 -0300

    Script de importação de contatos correção do icone voltar (createUser.html e forgot.html);

--- Halle/platforms/android/assets/www/services/services.js
+++ Halle/platforms/android/assets/www/services/services.js
@@ -64,99 +64,113 @@ app.service('PushNotificationService', function($http, $rootScope) {
 app.service('PhoneService', function() {
 
   var contato = "";
-  this.contactPattern = function(numContato, ddi, ddd) {
+
+  function ContatoPadrao(numContato, ddi, ddd) {
+       /* ------------------------------------------------------
+       ddi - DDI padrão dever seguir o seguinte exemplo "+55" ( padrão da função +55 - Brasil)
+       ddd - DDD padrão dever seguir o seguinte exemplo "11" ( padrão da função +21 - Rio de janeiro)
+       numcontato - numero de telefone que será formatado
+      --------------------------------------------------------*/
+      "use strict";
+      var cel, contato = "";
+      //alert(numContato);
+
       /* ------------------------------------------------------
-      ddi - DDI padrão dever seguir o seguinte exemplo "+55" ( padrão da função +55 - Brasil)
-      ddd - DDD padrão dever seguir o seguinte exemplo "11" ( padrão da função +21 - Rio de janeiro)
-      numcontato - numero de telefone que será formatado
-     --------------------------------------------------------*/
-     "use strict";
-     var cel, contato = "";
-     //alert(numContato);
+         Tratamento do ddd
+      --------------------------------------------------------*/
+  	  if (ddd.length > 2) {
+          ddd = ddd.slice(-2);
+      }
+      if (ddd.length < 2) {
+          ddd = '21';     // ddd padrao
+      }
 
-     /* ------------------------------------------------------
-        Tratamento do ddd
-     --------------------------------------------------------*/
-     if (ddd.length > 2) {
-         ddd = ddd.slice(-2);
-     }
-     if (ddd.length < 2) {
-         ddd = '21';     // ddd padrao
-     }
+  	  if ( isNaN(pos)){
+             ddd = '21';
+      }
 
-     /* ------------------------------------------------------
-         Tratamento do ddi
-     --------------------------------------------------------*/
-     if (ddi.length === 2) {
-         ddi = '+' + ddi;
-     }
-     if (ddi.length !== 3 || ddi.substring(0,1) != '+') {
-          ddi = '+55';
-     }
+      /* ------------------------------------------------------
+          Tratamento do ddi
+      --------------------------------------------------------*/
+      if (ddi.length === 2) {
+          ddi = '+' + ddi;
+      }
 
-     /* ------------------------------------------------------
-         Tratamento do contato
-     --------------------------------------------------------*/
+      if (ddi.length !== 3 ||
+  		ddi.substring(0,1) != '+' ||
+  		isNaN(ddi.substring(1,3)))   {
+  		    ddi = '+55';
+      }
 
-     // Vericar se o DDI existe no contato identificando o +
-     if (numContato.substring(0,1) == "+"){
-         ddi = numContato.substring(0,3);
-         numContato=numContato.substring(3,100);
-     }
+    	if (ddi == '+ ' || ddi == '+  ') {
+    		ddi = '+55';
+      }
 
-     // Limpar a string do contato e manter apenas numero
-     var pos;
-     var i =0;
-     for (i = 0; i < numContato.length; i++) {
-         pos = numContato.substring (i,i+1);
-         if ( !isNaN(pos) && pos != " " ){
-             contato = contato + pos;
-         }
-     }
+      /* ------------------------------------------------------
+          Tratamento do contato
+      --------------------------------------------------------*/
 
-     //contato= limparContato(numContato);
-     switch (contato.length) {
-     case  8:
-         cel = contato;
-         break;
-     case  9:
-         cel = contato;
-         break;
-     case 10:     // tratamento 8 digitos
-         cel = contato.slice(-8);
-         ddd = contato.slice(-10,-8);
-         break;
-     case 13:     // tratamento 8 digitos
-         cel = contato.slice(-8);
-         ddd = contato.slice(-10, -8);
-         break;
-     case 12:
-         cel = contato.slice(-9);
-         ddd = contato.slice(-11, -9);
-         break;
-     case 14:
-         cel = contato.slice(-9);
-         ddd = contato.slice(-11, -9);
-         break;
-     case 11:
-         if (contato.substring(0,1) == "0"){
-             cel = contato.slice(-8);          // tratamento 8 digitos
-             ddd = contato.slice(-10, -8);
-         } else {
-             cel = contato.slice(-9);
-             ddd = contato.slice(-11, -9);
-         }
-         break;
-     default:
-         cel='';
-     }
-     contato= ddi + ddd + cel;
-     if (contato.length <13 || contato.length >14) {
-        contato ='';
-     }
+      // Vericar se o DDI existe no contato identificando o +
+      if (numContato.substring(0,1) == "+"){
+          ddi = numContato.substring(0,3);
+          numContato=numContato.substring(3,100);
+      }
 
-     return contato;
+      // Limpar a string do contato e manter apenas numero
+      var pos;
+      var i =0;
+      for (i = 0; i < numContato.length; i++) {
+          pos = numContato.substring (i,i+1);
+          if ( !isNaN(pos) && pos != " " ){
+              contato = contato + pos;
+          }
+      }
+
+      //contato= limparContato(numContato);
+      switch (contato.length) {
+      case  8:
+          cel = contato;
+          break;
+      case  9:
+          cel = contato;
+          break;
+      case 10:     // tratamento 8 digitos
+          cel = contato.slice(-8);
+          ddd = contato.slice(-10,-8);
+          break;
+      case 13:     // tratamento 8 digitos
+          cel = contato.slice(-8);
+          ddd = contato.slice(-10, -8);
+          break;
+      case 12:
+          cel = contato.slice(-9);
+          ddd = contato.slice(-11, -9);
+          break;
+      case 14:
+          cel = contato.slice(-9);
+          ddd = contato.slice(-11, -9);
+          break;
+      case 11:
+          if (contato.substring(0,1) == "0"){
+              cel = contato.slice(-8);          // tratamento 8 digitos
+              ddd = contato.slice(-10, -8);
+          } else {
+              cel = contato.slice(-9);
+              ddd = contato.slice(-11, -9);
+          }
+          break;
+      default:
+          cel='';
+      }
+      contato= ddi + ddd + cel;
+
+      if (contato.length <13 || contato.length >14) {
+         contato ='';
+      }
+
+      return contato;
   }
+
   return contato;
 });
 
