7c5a96f8212ef549f18426ca9510ef6972123ccd
lbaiao <leonardobaiao@gmail.com>
2016-09-06 19:29:12 -0300

    Nova versão

--- halle/www/resources/resources.js
+++ halle/www/resources/resources.js
@@ -2,9 +2,9 @@ var app = angular.module('halleApp.resources', ['ngResource']);
 
 /****************Constantes ***********************/
 app.constant('ApiEndpoint', {
-  url: 'http://192.241.158.95/HalleWEB/service'
+  //url: 'http://192.241.158.95/HalleWEB/service'
   //url: 'https://apidev.halleapp.net/HalleWEB/service'
-  //url: 'https://api.halleapp.net/HalleWEB/service'
+  url: 'https://api.halleapp.net/HalleWEB/service'
 });
 
 
